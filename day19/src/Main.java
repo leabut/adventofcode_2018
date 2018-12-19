7cbc81ccd4677f1624d0d223a10bdc4a45dd2d96
Kris Wolfgramm <kris.wolfgramm@student.uni-teubingen.de>
2018-12-19 21:41:33 +0100

    added day19, working

--- day19/src/Main.java
+++ day19/src/Main.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 
 public class Main {
 
-	static String[] allLines = new String[7];
+	static String[] allLines = new String[37-1];
 	static int[] registers = new int[6];
 	static int pointer = 0;
 
 	public static void main(String[] args) throws IOException {
 		@SuppressWarnings("resource")
-		BufferedReader br = new BufferedReader(new FileReader("example.txt"));
+		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
 		String line;
 
 		for (int i = 0; ((line = br.readLine()) != null); i++) {
@@ -20,10 +20,16 @@ public class Main {
 			allLines[i - 1] = line;
 		}
 
+		//registers[0] = 1;
 		while (pointer < allLines.length) {
+			registers[1] = pointer;
 			execute();
-			pointer = registers[0];
+			pointer = registers[1];
 			pointer++;
+		}
+
+		for (int i = 0; i < registers.length; i++) {
+			System.out.print(registers[i] + " ");
 		}
 	}
 
@@ -33,16 +39,80 @@ public class Main {
 
 		switch (split[0]) {
 		case "seti":
-			 registers[Integer.parseInt(split[3])] = Integer.parseInt(split[1]);
+			registers[Integer.parseInt(split[3])] = Integer.parseInt(split[1]);
 			break;
 		case "setr":
-			 registers[Integer.parseInt(split[3])] = registers[Integer.parseInt(split[1])];
+			registers[Integer.parseInt(split[3])] = registers[Integer.parseInt(split[1])];
 			break;
 		case "addi":
-			registers[Integer.parseInt(split[3])] += Integer.parseInt(split[1]);
+			registers[Integer.parseInt(split[3])] = registers[Integer.parseInt(split[1])] + Integer.parseInt(split[2]);
 			break;
 		case "addr":
-			registers[Integer.parseInt(split[3])] += registers[Integer.parseInt(split[1])];
+			registers[Integer.parseInt(split[3])] = registers[Integer.parseInt(split[1])]
+					+ registers[Integer.parseInt(split[2])];
+			break;
+		case "muli":
+			registers[Integer.parseInt(split[3])] = registers[Integer.parseInt(split[1])] * Integer.parseInt(split[2]);
+			break;
+		case "mulr":
+			registers[Integer.parseInt(split[3])] = registers[Integer.parseInt(split[1])]
+					* registers[Integer.parseInt(split[2])];
+			break;
+		case "bani":
+			registers[Integer.parseInt(split[3])] = registers[Integer.parseInt(split[1])] & Integer.parseInt(split[2]);
+			break;
+		case "banr":
+			registers[Integer.parseInt(split[3])] = registers[Integer.parseInt(split[1])]
+					& registers[Integer.parseInt(split[2])];
+			break;
+		case "bori":
+			registers[Integer.parseInt(split[3])] = registers[Integer.parseInt(split[1])] | Integer.parseInt(split[2]);
+			break;
+		case "borr":
+			registers[Integer.parseInt(split[3])] = registers[Integer.parseInt(split[1])]
+					| registers[Integer.parseInt(split[2])];
+			break;
+		case "gtir":
+			if (Integer.parseInt(split[1]) > registers[Integer.parseInt(split[2])]) {
+				registers[Integer.parseInt(split[3])] = 1;
+			} else {
+				registers[Integer.parseInt(split[3])] = 0;
+			}
+			break;
+		case "gtri":
+			if (registers[Integer.parseInt(split[1])] > Integer.parseInt(split[2])) {
+				registers[Integer.parseInt(split[3])] = 1;
+			} else {
+				registers[Integer.parseInt(split[3])] = 0;
+			}
+			break;
+		case "gtrr":
+			if (registers[Integer.parseInt(split[1])] > registers[Integer.parseInt(split[2])]) {
+				registers[Integer.parseInt(split[3])] = 1;
+			} else {
+				registers[Integer.parseInt(split[3])] = 0;
+			}
+			break;
+		case "eqir":
+			if (Integer.parseInt(split[1]) == registers[Integer.parseInt(split[2])]) {
+				registers[Integer.parseInt(split[3])] = 1;
+			} else {
+				registers[Integer.parseInt(split[3])] = 0;
+			}
+			break;
+		case "eqri":
+			if (registers[Integer.parseInt(split[1])] == Integer.parseInt(split[2])) {
+				registers[Integer.parseInt(split[3])] = 1;
+			} else {
+				registers[Integer.parseInt(split[3])] = 0;
+			}
+			break;
+		case "eqrr":
+			if (registers[Integer.parseInt(split[1])] == registers[Integer.parseInt(split[2])]) {
+				registers[Integer.parseInt(split[3])] = 1;
+			} else {
+				registers[Integer.parseInt(split[3])] = 0;
+			}
 			break;
 		default:
 			System.out.println("Should never happen");
